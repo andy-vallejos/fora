c9f3b64e1d1c718de7a580b66f8c4436db7ab681
Andy Gildo Vallejos Bascope <andyvallejosgb@gmail.com>
2026-06-11 21:33:36 -0400

    fix: agregue el beforeEach en los tests de alojamiento

--- src/AlojamientoPromedioTest.java
+++ src/AlojamientoPromedioTest.java
@@ -5,54 +5,26 @@ import org.junit.jupiter.api.Test;
 
 public class AlojamientoPromedioTest
 {
-    @Test
-    public void testAlojamientoNuevoSinReseniasDebeRetornarCero() {
-
-        Alojamiento alojamientoNuevo = new Alojamiento("Cochabamba", "Departamento", null, 150);
-
-        double promedioObtenido = alojamientoNuevo.promedioDeResenias();
-
-        assertEquals(0.0, promedioObtenido);
+    private Alojamiento alojamiento;
+    @BeforeEach public void setUp() {
+        alojamiento = new Alojamiento("Cochabamba", "Departamento", null, 150);
     }
+
+    @Test
+    public void testAlojamientoNuevoSinReseniasDebeRetornarCero() {  double promedioObtenido = alojamiento.promedioDeResenias(); assertEquals(0.0, promedioObtenido); }
 
     @Test
     public void testAlojamientoConUnaReseniaDebeRetornarMismoValorExacto() {
-
-        Alojamiento alojamientoConUnaResenia = new Alojamiento("Cochabamba", "Habitación", null, 80);
-        Resenia primeraResenia = new Resenia(4, "Excelente atención del propietario");
-
-        alojamientoConUnaResenia.agregarResenia(primeraResenia);
-        double promedioObtenido = alojamientoConUnaResenia.promedioDeResenias();
-
-        assertEquals(4.0, promedioObtenido);
-    }
+        Resenia primeraResenia = new Resenia(4, "Excelente atención");
+        alojamiento.agregarResenia(primeraResenia); double promedioObtenido = alojamiento.promedioDeResenias();
+        assertEquals(4.0, promedioObtenido); }
 
     @Test
     public void testMultiplesReseniasDivisionExactaDebeRetornarDecimalExacto() {
-
-        Alojamiento alojamiento = new Alojamiento("Cochabamba", "Departamento", null, 200);
-        Resenia r1 = new Resenia(5, "Increíble");
-        Resenia r2 = new Resenia(4, "Bueno");
-
-        alojamiento.agregarResenia(r1);
-        alojamiento.agregarResenia(r2);
-        double promedioObtenido = alojamiento.promedioDeResenias();
-
-        assertEquals(4.5, promedioObtenido);
-    }
+        Resenia r1 = new Resenia(5, "Increíble"); Resenia r2 = new Resenia(4, "Bueno"); alojamiento.agregarResenia(r1); alojamiento.agregarResenia(r2); double promedioObtenido = alojamiento.promedioDeResenias();
+        assertEquals(4.5, promedioObtenido); }
 
     @Test
-    public void testMultiplesReseniasPeriodicasDebeRetornarRedondeoDosDecimales() {
-        Alojamiento alojamiento = new Alojamiento("Cochabamba", "Habitación", null, 90);
-        Resenia r1 = new Resenia(4, "Aceptable");
-        Resenia r2 = new Resenia(4, "Ok");
-        Resenia r3 = new Resenia(5, "Me gustó mucho");
-
-        alojamiento.agregarResenia(r1);
-        alojamiento.agregarResenia(r2);
-        alojamiento.agregarResenia(r3);
-        double promedioObtenido = alojamiento.promedioDeResenias();
-
-        assertEquals(4.33, promedioObtenido);
-    }
+    public void testMultiplesReseniasPeriodicasDebeRetornarRedondeoDosDecimales() {Resenia r1 = new Resenia(4, "Aceptable"); Resenia r2 = new Resenia(4, "Ok"); Resenia r3 = new Resenia(5, "Me gustó mucho"); alojamiento.agregarResenia(r1); alojamiento.agregarResenia(r2); alojamiento.agregarResenia(r3); double promedioObtenido = alojamiento.promedioDeResenias();
+        assertEquals(4.33, promedioObtenido); }
 }
