a97d6d0cdccf36ad9710c89c3c6f094b430a62d1
Andy Gildo Vallejos Bascope <andyvallejosgb@gmail.com>
2026-06-11 20:43:15 -0400

    feat: agregue el beforeeach a las pruebas de usuario

--- src/UsuarioTest.java
+++ src/UsuarioTest.java
@@ -1,20 +1,25 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UsuarioTest {
+    Usuario usuario;
+
+    @BeforeEach
+    public void setup(){
+        usuario = new Usuario("Andy Vallejos", "email", 69442181);
+    }
     @Test
     public void crearUsuario(){
-        Usuario u = new Usuario("Andy Vallejos", "email", 69442181);
-        assertEquals("Andy Vallejos", u.getNombre());
-        assertEquals("email", u.getCorreo());
-        assertEquals(69442181, u.getTelefono());
+        assertEquals("Andy Vallejos", usuario.getNombre());
+        assertEquals("email", usuario.getCorreo());
+        assertEquals(69442181, usuario.getTelefono());
     }
 
     @Test
     public void crearUsuarioSinNombre(){
-        Usuario u = new Usuario("   ", "email", 69442181);
-        assertEquals("email", u.getCorreo());
-        assertFalse(u.getNombre().isEmpty());
+        assertEquals("email", usuario.getCorreo());
+        assertFalse(usuario.getNombre().isEmpty());
     }
 
     @Test
