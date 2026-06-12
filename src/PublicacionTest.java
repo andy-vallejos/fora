b3948684b7c52010f237d0794057c7de5c1b773c
Andy Gildo Vallejos Bascope <andyvallejosgb@gmail.com>
2026-06-11 20:37:41 -0400

    feat: agregue el before each a las pruebas de publicacion

--- src/PublicacionTest.java
+++ src/PublicacionTest.java
@@ -1,21 +1,27 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
 public class PublicacionTest {
+    private Usuario usuario;
+    private Alojamiento alojamiento;
+    Publicacion publicacion;
+
+    @BeforeEach
+    public void setUp(){
+        usuario = new Usuario("Andy Vallejos", "email", 69442181);
+        alojamiento = new Alojamiento("sacaba", "departamento", usuario, 300);
+        publicacion = new Publicacion(alojamiento);
+    }
+
     @Test
     public void denunciarPublicacion(){
-        Usuario u = new Usuario("Andy Vallejos", "email", 69442181);
-        Alojamiento alojamiento = new Alojamiento("sacaba", "departamento", u, 300);
-        Publicacion publicacion = new Publicacion(alojamiento);
-
         assertEquals(0, publicacion.getDenuncias());
     }
 
     @Test
-    public void reportarPublicacion2Veces(){
-        Usuario u = new Usuario("Andy Vallejos", "email", 69442181);
-        Alojamiento alojamiento = new Alojamiento("sacaba", "departamento", u,300);
-        Publicacion publicacion = new Publicacion(alojamiento);
-
+    public void denunciarPublicacion2Veces(){
         publicacion.denunciar();
         publicacion.denunciar();
         assertEquals(2, publicacion.getDenuncias());
@@ -23,20 +29,12 @@ public class PublicacionTest {
 
     @Test
     public void publicacionDebeEstarDesbloqueadaConMenosDeCincoDenuncias() {
-        Usuario u = new Usuario("Andy Vallejos", "email", 69442181);
-        Alojamiento alojamiento = new Alojamiento("sacaba", "departamento", u, 300);
-        Publicacion publicacion = new Publicacion(alojamiento);
-
         assertFalse(publicacion.estaBloqueada());
     }
 
 
     @Test
     public void publicacionDebeBloquearseConCincoDenuncias() {
-        Usuario u = new Usuario("Andy Vallejos", "email", 69442181);
-        Alojamiento alojamiento = new Alojamiento("sacaba", "departamento", u, 300);
-        Publicacion publicacion = new Publicacion(alojamiento);
-
         for (int i = 0; i < 5; i++) {
             publicacion.denunciar();
         }
@@ -46,49 +44,25 @@ public class PublicacionTest {
 
     @Test
     public void testDeberiaClasificarComoZonaSegura() {
-
-        Usuario usuario = new Usuario("Alan Poma", "alanpoma@gmail.com", 76543210);
-        Alojamiento alojamiento = new Alojamiento("Zona Sur", "Hotel", usuario, 300);
-        Publicacion publicacion = new Publicacion(alojamiento);
-
         ReporteSeguridad reporte = new ReporteSeguridad(8, 8);
-
         publicacion.agregarReporteSeguridad(reporte);
-
         String resultado = publicacion.clasificarZona();
-
         assertEquals("Segura", resultado);
     }
 
     @Test
     public void testDeberiaClasificarComoZonaRegular() {
-
-        Usuario usuario = new Usuario("Alan Poma", "alanpoma@gmail.com", 76543210);
-        Alojamiento alojamiento = new Alojamiento("Zona Sur", "Hotel", usuario, 300);
-        Publicacion publicacion = new Publicacion(alojamiento);
-
         ReporteSeguridad reporte = new ReporteSeguridad(6, 6);
-
         publicacion.agregarReporteSeguridad(reporte);
-
         String resultado = publicacion.clasificarZona();
-
         assertEquals("Regular", resultado);
     }
 
     @Test
     public void testDeberiaClasificarComoZonaPeligrosa() {
-
-        Usuario usuario = new Usuario("Alan Poma", "alanpoma@gmail.com", 76543210);
-        Alojamiento alojamiento = new Alojamiento("Zona Sur", "Hotel", usuario, 300);
-        Publicacion publicacion = new Publicacion(alojamiento);
-
         ReporteSeguridad reporte = new ReporteSeguridad(2, 2);
-
         publicacion.agregarReporteSeguridad(reporte);
-
         String resultado = publicacion.clasificarZona();
-
         assertEquals("Peligrosa", resultado);
     }
 }
